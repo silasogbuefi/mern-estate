a83084b7ee7182327cc81dbe0f8c554358b36357
segun adebayo <eniolaadebayo24@gmail.com>
2023-12-23 16:39:43 +0100

    connect to database

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
             
             <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
                 <input type="text" placeholder='search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
-                <FaSearch className='text-slate-500' />
+                <FaSearch className='text-slate-600' />
             </form>
             <ul className='flex gap-4'>
                 <Link to='/'>
@@ -24,7 +24,9 @@ const Header = () => {
                 <li className='hidden sm:inline text-slate-700 hover:underline'>about</li>
                 </Link>
                 <Link to='sign-in'>
-                <li className=' text-slate-700 hover:underline'>sign in</li>
+                <li className=' text-slate-700 hover:underline'>
+                    sign in
+                </li>
                 </Link>
             </ul>
         </div>
